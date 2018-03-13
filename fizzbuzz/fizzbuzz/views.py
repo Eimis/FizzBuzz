e26a97868edcf29894d693f08a558c1a6b05f023
Eimantas <eimantas.stonys@gmail.com>
2018-03-13 10:36:50 +0100

    Updated main template to list all Generators

--- fizzbuzz/fizzbuzz/views.py
+++ fizzbuzz/fizzbuzz/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from fizzbuzz.models import Generator
+
 
 def main(request):
     '''Main app view
     '''
 
-    return render(request, 'main.html', {})
+    return render(request, 'main.html', {
+        'generators': Generator.objects.all(),
+    })
