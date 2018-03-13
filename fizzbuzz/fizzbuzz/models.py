3d763db2e717f454d5e669e182502b9a5d0597b6
Eimantas <eimantas.stonys@gmail.com>
2018-03-13 10:23:40 +0100

    Add new 'results_per_generator' field to Generator model

--- fizzbuzz/fizzbuzz/models.py
+++ fizzbuzz/fizzbuzz/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Generator(models.Model):
     name = models.CharField(max_length=20)
+    results_per_generator = models.IntegerField(
+        help_text=_(
+            'Since the length of generated string for a generator is '
+            'unlimited, we have to set a maximum numberof results to return. '
+        )
+    )
 
     def __unicode__(self):
         return self.name
