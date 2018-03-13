3c6c9127544d863d71e2d0c4f3099eaf7c119fd4
Eimantas <eimantas.stonys@gmail.com>
2018-03-13 11:25:09 +0100

    Added view for a Generator object

--- fizzbuzz/fizzbuzz/urls.py
+++ fizzbuzz/fizzbuzz/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from fizzbuzz.views import main
+from fizzbuzz.views import generator_view, main
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', main),
+    url(
+        r'^generator/(?P<hash_for_url>.*)$',
+        generator_view,
+        name='generator_view'
+    ),
 ]
--- fizzbuzz/fizzbuzz/views.py
+++ fizzbuzz/fizzbuzz/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
 
 from fizzbuzz.models import Generator
 
@@ -9,4 +10,20 @@ def main(request):
 
     return render(request, 'main.html', {
         'generators': Generator.objects.all(),
+    })
+
+
+def generator_view(request, hash_for_url):
+    '''A view for using Generator and generating sequences
+    '''
+
+    errors = None
+    generator = Generator.objects.filter(hash_for_url=hash_for_url).first()
+
+    if not generator:
+        errors = _("Generator with such URL doesn't exist!")
+
+    return render(request, 'generator.html', {
+        'errors': errors,
+        'generator': generator,
     })
