8815a6d51b331aec435358240bce0cdea67a4087
Eimantas <eimantas.stonys@gmail.com>
2018-03-13 10:12:57 +0100

    Show new Divisor and String models in Django admin

--- fizzbuzz/fizzbuzz/admin.py
+++ fizzbuzz/fizzbuzz/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from fizzbuzz.models import Generator
+from fizzbuzz.models import Divisor, Generator, String
+
+
+class DivisorAdmin(admin.ModelAdmin):
+    pass
 
 
 class GeneratorAdmin(admin.ModelAdmin):
     pass
 
+
+class StringAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Divisor, DivisorAdmin)
 admin.site.register(Generator, GeneratorAdmin)
+admin.site.register(String, StringAdmin)
